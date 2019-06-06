52fd39c40056c9ca4e8a8945eb998cf5ace30a8c
John Hany <johnhany@163.com>
2019-06-07 03:01:03 +0800

    rename losses

--- loss.py
+++ loss.py
@@ -12,24 +12,24 @@ class GeneratorLoss(nn.Module):
             param.requires_grad = False
         self.loss_network = loss_network
         self.mse_loss = nn.MSELoss()
-        self.tv_loss = TVLoss()
+        self.l2_loss = L2Loss()
 
     def forward(self, out_labels, out_images, target_images):
-        # Adversarial Loss
+        # adversarial Loss
         adversarial_loss = torch.mean(1 - out_labels)
-        # Perception Loss
-        perception_loss = self.mse_loss(self.loss_network(out_images), self.loss_network(target_images))
-        # Image Loss
-        image_loss = self.mse_loss(out_images, target_images)
-        # TV Loss
-        tv_loss = self.tv_loss(out_images)
-        return image_loss + 0.001 * adversarial_loss + 0.006 * perception_loss + 2e-8 * tv_loss
+        # vgg Loss
+        vgg_loss = self.mse_loss(self.loss_network(out_images), self.loss_network(target_images))
+        # pixel-wise Loss
+        pixel_loss = self.mse_loss(out_images, target_images)
+        # regularization Loss
+        reg_loss = self.tv_loss(out_images)
+        return pixel_loss + 0.001 * adversarial_loss + 0.006 * vgg_loss + 2e-8 * reg_loss
 
 
-class TVLoss(nn.Module):
-    def __init__(self, tv_loss_weight=1):
-        super(TVLoss, self).__init__()
-        self.tv_loss_weight = tv_loss_weight
+class L2Loss(nn.Module):
+    def __init__(self, l2_loss_weight=1):
+        super(L2Loss, self).__init__()
+        self.l2_loss_weight = l2_loss_weight
 
     def forward(self, x):
         batch_size = x.size()[0]
@@ -37,9 +37,9 @@ class TVLoss(nn.Module):
         w_x = x.size()[3]
         count_h = self.tensor_size(x[:, :, 1:, :])
         count_w = self.tensor_size(x[:, :, :, 1:])
-        h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
-        w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
-        return self.tv_loss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+        h_l2 = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
+        w_l2 = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
+        return self.l2_loss_weight * 2 * (h_l2 / count_h + w_l2 / count_w) / batch_size
 
     @staticmethod
     def tensor_size(t):
